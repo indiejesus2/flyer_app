b480afac6696bd7bdfcc518362d158530f2b055f
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-09-27 18:09:49 -0400

    WIP: Built out new, show, and index for Venues.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,25 @@
 class VenuesController < ApplicationController
+
+    def index
+        @venues = Venue.all
+    end
+
+    def new
+        @venue = Venue.new
+    end
+
+    def create
+        @venue = Venue.create(venue_params)
+    end
+
+    def show
+        @venue = Venue.find_by_id(params[:id])
+    end
+
+    private
+
+    def venue_params
+        params.require(:venue).permit(:name, :location, :crowd_capacity, :parking)
+    end
+
 end
