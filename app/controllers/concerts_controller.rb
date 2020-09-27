39c69a38ddcd7f967634cbbc8f4afe383f690b62
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-09-27 19:24:56 -0400

    WIP: Added new and create to concerts controller. Also added validation for date on concert model.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,18 @@
 class ConcertsController < ApplicationController
+
+    def new
+        @concert = Concert.new
+    end
+
+    def create
+        @concert = Concert.new
+
+        if @concert.save
+            render conert_part(@conert)
+        else
+            redirect_to :new
+        end
+
+    end
+    
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,5 @@
 class Concert < ApplicationRecord
   belongs_to :band
   belongs_to :venue
+  validates :date, presence: :true
 end
