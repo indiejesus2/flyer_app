e81669e5770572e005236caaddc24f16de7f5537
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-09-27 22:57:56 -0400

    WIP: Created new form for concerts. Included nested resource to venue.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,17 +2,33 @@ class ConcertsController < ApplicationController
 
     def new
         @concert = Concert.new
+        @venue_id = params[:venue_id]
     end
 
     def create
-        @concert = Concert.new
+        @concert = Concert.new(concert_params)
 
         if @concert.save
-            render conert_part(@conert)
+            render concert_part(@concert)
         else
             redirect_to :new
         end
 
     end
-    
+
+    def show
+        @concert = Concert.find(params[:id])
+    end
+
+    def edit
+        @concert = Concert.find(params[:id])
+        @venue_id = params[:venue_id]
+    end
+
+    private
+
+    def concert_params
+        params.require(:concert).permit(:date, :admission, :venue_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :concerts
+
   resources :bands
-  resources :venues
+  resources :venues do
+    resources :concerts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
