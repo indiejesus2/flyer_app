1ce48d4ff2600d4c561f9aac9c9d1b80e98bd276
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-10-05 20:45:03 -0400

    WIP: Added filter capabilities for concerts by band, venue and date.

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,4 +3,18 @@ class Concert < ApplicationRecord
   belongs_to :venue
   validates :date, presence: :true
   accepts_nested_attributes_for :venue, reject_if: :all_blank
+
+  def self.by_band(band)
+    band = Band.find_by(name: band)
+    where(band: band.id)
+  end
+
+  def self.by_venue(venue_id)
+    where(venue: venue_id)
+  end
+
+  def self.by_date(date)
+    where(concerts: {date: date})
+  end
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,13 @@
 class Venue < ApplicationRecord
     has_many :concerts
     has_many :bands, through: :concerts
+
+    def location
+        city + ", " + state
+    end
+
+    def search
+        name + " - " + location
+    end
+
 end
